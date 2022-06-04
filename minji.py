672cfb8dd98b535c0017304c6cf31d250d49a233
min-zzwi <101248834+min-zzwi@users.noreply.github.com>
2022-06-05 00:26:05 +0900

    Add files via upload

---
+++ minji.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver = webdriver.Chrome(executable_path="./chromedriver.exe")
+url = "https://www.musinsa.com/app/?NaPm=ct%3Dl3yedcow%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3D%7Chk%3De1ef83b897edf0a12e4c97045473642126a4fdb5"
+driver.get(url)
+
+time.sleep(3)
+
+serch = driver.find_element_by_xpath(
+    '//*[@id="search_query"]')
+serch.click()
+driver.find_element_by_xpath(
+    '//*[@id="search_query"]').send_keys('남성 상의')
+serch.send_keys(Keys.ENTER)
+
+res = []
+
+time.sleep(2)
+for j in range(1, 11):
+    try:
+        brand_name = driver.find_element_by_xpath(
+            f'//*[@id="searchList"]/li[{j}]/div[4]/div[2]/p[1]/a').text
+        print(f'---{brand_name}---')
+    except:
+        name = "NAN"
+        pass
+        
+        res.append(brand_name)
+print(res)
+driver.quit()
